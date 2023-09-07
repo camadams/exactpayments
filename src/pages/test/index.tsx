f9a1f822a345c798cb989d981dc133b4be86a971
cameron.adams <camgadams@gmail.com>
2023-09-07 07:26:37 +0200

    fix eslint build error

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -21,42 +21,46 @@ const customers = [
 type Grid = number[][]; // Define your grid data type
 
 function GridTable() {
-  const [grid, setGrid] = useState<Grid>([
-    [2, 2, 3, 4, 5, 6, 7, 8, 9, 0],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
-  ]);
+  const [grid, setGrid] = useState<Grid>([[...new Array<number>(customers.length * products.length).fill(0)], [...new Array<number>(customers.length * products.length).fill(0)]]);
   const [activeCell, setActiveCell] = useState<Cell>({ row: -1, col: -1 });
   const tableRef: RefObject<HTMLTableElement> = React.createRef();
 
-  // useEffect(() => {
-  //   // Add event listener to synchronize horizontal scrolling of header and data tables
-  //   const handleScroll = () => {
-  //     // if (tableRef.current) {
-  //     //   const headerTable = tableRef.current.querySelector(".header-table");
-  //     //   const dataTable = tableRef.current.querySelector(".data-table");
-  //     //   if (headerTable && dataTable) {
-  //     //     headerTable.scrollLeft = dataTable.scrollLeft;
-  //     //   }
-  //     // }
-  //   };
+  useEffect(() => {
+    const handleArrowKeys = (event: KeyboardEvent) => {
+      if (activeCell.row === -1 || activeCell.col === -1) return;
 
-  //   if (tableRef.current) {
-  //     tableRef.current.addEventListener("scroll", handleScroll);
-  //   }
+      const handleNavigation = (rowChange: number, colChange: number) => {
+        const newRow = activeCell.row + rowChange;
+        const newCol = activeCell.col + colChange;
 
-  //   return () => {
-  //     if (tableRef.current) {
-  //       tableRef.current.removeEventListener("scroll", handleScroll);
-  //     }
-  //   };
-  // }, []);
+        if (newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0]!.length) {
+          setActiveCell({ row: newRow, col: newCol });
+        }
+      };
+
+      switch (event.key) {
+        case "ArrowUp":
+          handleNavigation(-1, 0);
+          break;
+        case "ArrowDown":
+          handleNavigation(1, 0);
+          break;
+        case "ArrowLeft":
+          handleNavigation(0, -1);
+          break;
+        case "ArrowRight":
+          handleNavigation(0, 1);
+          break;
+        default:
+          return;
+      }
+    };
+
+    window.addEventListener("keydown", handleArrowKeys);
+    return () => {
+      window.removeEventListener("keydown", handleArrowKeys);
+    };
+  }, [activeCell, grid]);
 
   const handleCellClick = (rowIndex: number, colIndex: number) => {
     setActiveCell({ row: rowIndex, col: colIndex });
@@ -64,20 +68,35 @@ function GridTable() {
 
   const handleCellValueChange = (event: ChangeEvent<HTMLInputElement>, rowIndex: number, colIndex: number) => {
     const updatedGrid = [...grid];
-    const newValue = Number(event.target.value);
-
-    if (!isNaN(newValue)) {
-      updatedGrid[rowIndex]![colIndex] = newValue;
-      setGrid(updatedGrid);
-    }
+    const newValue = parseInt(event.target.value);
+    updatedGrid[rowIndex]![colIndex] = newValue;
+    setGrid(updatedGrid);
   };
 
+  const handleOnBlur = () => {
+    if (isNaN(grid[activeCell.row]![activeCell.col]!)) {
+      const updatedGrid = [...grid];
+      updatedGrid[activeCell.row]![activeCell.col] = 0;
+      setGrid(updatedGrid);
+    }
+
+    setActiveCell({ row: -1, col: -1 });
+  };
+
+  // const updateGridCell = (rowIndex: number, colIndex: number, val: any) => {
+  //   const updatedGrid = [...grid];
+  //   updatedGrid[rowIndex]![colIndex] = Number(val);
+  //   setGrid(updatedGrid);
+  // };
+
+  // const x = [0, 0, 0, 0];
+  const numOfCustomers = new Array<number>(customers.length);
   return (
     <div className="bg-gray-400 p-4 ">
       <div className="flex ">
         <div className="w-1/12 bg-red-300">Hi</div>
         <div className=" w-full" style={{ overflowX: "auto", scrollbarGutter: "stable" }}>
-          <div className="pr-3" style={{ scrollbarGutter: "stable" }}>
+          <div className="pr-4" style={{ scrollbarGutter: "stable" }}>
             <div className="flex ">
               {customers.map((val, i) => (
                 <div key={i} className="border  w-full">
@@ -86,10 +105,10 @@ function GridTable() {
               ))}
             </div>
             <div className="flex">
-              {[...Array(customers.length)].map((val, j) => (
+              {[...numOfCustomers].map((_, j) => (
                 <div key={j} className="flex" style={{ width: `${100 / customers.length}%` }}>
                   {products.map((val, i) => (
-                    <div key={i} className="border text-[0.6rem]" style={{ width: `${100 / products.length}%` }}>
+                    <div key={i} className="border text-sm" style={{ width: `${100 / products.length}%` }}>
                       {val.name}
                     </div>
                   ))}
@@ -97,7 +116,7 @@ function GridTable() {
               ))}
             </div>
           </div>
-          <div className="table-container" style={{ overflowY: "auto", maxHeight: "400px" }}>
+          <div className="table-container" style={{ overflowY: "auto", maxHeight: "600px" }}>
             <table className="table-fixed border-collapse border data-table w-full">
               <tbody>
                 {grid.map((row, rowIndex) => (
@@ -105,16 +124,16 @@ function GridTable() {
                     {row.map((cell, colIndex) => (
                       <td
                         key={colIndex}
-                        className={`border px-6 py-4 ${Math.floor(colIndex / products.length) % products.length === 0 ? "bg-zinc-500" : ""} `}
-                        onClick={() => handleCellClick(rowIndex + 1, colIndex)}
+                        className={`border p-1 ${Math.floor(colIndex / products.length) % products.length === 0 ? "bg-zinc-500" : ""} `}
+                        onClick={() => handleCellClick(rowIndex, colIndex)}
                       >
-                        {activeCell.row === rowIndex + 1 && activeCell.col === colIndex ? (
+                        {activeCell.row === rowIndex && activeCell.col === colIndex ? (
                           <input
-                            className="focus:outline-none appearance-none bg-gray-400 w-5"
+                            className="focus:outline-none appearance-none bg-gray-400 w-full"
                             type="number"
                             value={cell}
-                            onChange={(e) => handleCellValueChange(e, rowIndex + 1, colIndex)}
-                            onBlur={() => setActiveCell({ row: -1, col: -1 })}
+                            onChange={(e) => handleCellValueChange(e, rowIndex, colIndex)}
+                            onBlur={() => handleOnBlur()}
                             autoFocus
                           />
                         ) : (
